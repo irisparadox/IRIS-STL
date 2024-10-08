69409e72e1f9d08f8b4bf40b2a836e1c1f5e3004
irisparadox <gasaav01@ucm.es>
2024-10-08 19:41:20 +0200

    finished implementing iterators

--- MySTL/main.cpp
+++ MySTL/main.cpp
@@ -1,11 +1,16 @@
 #include <iarray>
-#include <vector>
-#include <array>
+#include <iostream>
 
 int main() {
 	IRIS::iarray<int, 5> arr = { 1, 2, 3, 4, 5 };
 
-	for(auto& val : )
+	for (auto& val : arr) {
+		val *= 4;
+	}
+
+	for (auto& val : arr) {
+		std::cout << val << '\n';
+	}
 
 	return 0;
 }
