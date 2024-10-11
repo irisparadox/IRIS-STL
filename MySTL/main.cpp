cdc67176fbd5d05f826182234f84d51c40bc5965
irisparadox <gasaav01@ucm.es>
2024-10-11 10:03:46 +0200

    vector hell 2 Electric boogaloo

--- MySTL/main.cpp
+++ MySTL/main.cpp
@@ -4,9 +4,24 @@
 #include <ivector>
 
 int main() {
-	std::vector<int> stdvec;
+	IRIS::ivector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	vec.push_back(6);
+	vec.push_back(7);
+	vec.push_back(8);
+	vec.push_back(9);
+	vec.push_back(10);
+	vec.push_back(11);
+	vec.push_back(12);
+	vec.push_back(13);
+	vec.push_back(14);
+	vec.push_back(15);
 
-	stdvec.push_back(1);
+	std::cout << vec.capacity() << '\n';
 
 	return 0;
 }
