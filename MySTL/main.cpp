1abb8c213b42ce36fdf1c66067e00ab7b6a977a8
irisparadox <gasaav01@ucm.es>
2024-10-13 01:16:23 +0200

    vector hell iterators

--- MySTL/main.cpp
+++ MySTL/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <ivector>
+#include <array>
 
 int main() {
 	IRIS::ivector<int> vec;
@@ -9,19 +10,11 @@ int main() {
 	vec.push_back(2);
 	vec.push_back(3);
 	vec.push_back(4);
-	vec.push_back(5);
-	vec.push_back(6);
-	vec.push_back(7);
-	vec.push_back(8);
-	vec.push_back(9);
-	vec.push_back(10);
-	vec.push_back(11);
-	vec.push_back(12);
-	vec.push_back(13);
-	vec.push_back(14);
-	vec.push_back(15);
+
+	std::cout << vec.front() << '\n';
+	std::cout << vec.back() << '\n';
 
 	std::cout << vec.capacity() << '\n';
-
+		
 	return 0;
 }
