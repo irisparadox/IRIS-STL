854a356f1ed2014a68915e498c90685e6881a7a1
irisparadox <gasaav01@ucm.es>
2024-10-13 20:40:29 +0200

    vector hell VARIADIC CONSTRUCTOR YAY

--- MySTL/main.cpp
+++ MySTL/main.cpp
@@ -5,11 +5,21 @@
 #include <array>
 
 int main() {
-	IRIS::ivector<int> vec;
+	IRIS::ivector<int> vec = { 1, 2, 3 };
 	vec.push_back(1);
 	vec.push_back(2);
 	vec.push_back(3);
 	vec.push_back(4);
+	
+	std::cout << vec[3] << '\n';
+
+	std::cout << "Evaluating vector\n";
+
+	for (auto& val : vec) {
+		std::cout << val;
+	}
+
+	std::cout << '\n';
 
 	std::cout << vec.front() << '\n';
 	std::cout << vec.back() << '\n';
