98b7a5e53e33091e53623a3074a4a382fb2ed7af
irisparadox <gasaav01@ucm.es>
2024-11-30 00:22:22 +0100

    Unordered_map WIP header doc

--- IRIS_STL/include/unordered_map.h
+++ IRIS_STL/include/unordered_map.h
@@ -1,3 +1,29 @@
+// unordered_map.h -*- C++ -*-
+//
+// Copyright (C) 2024 irisparadox
+//
+// This file is part of the IRIS::STL C++ Library, licensed under the MIT License.
+// You may obtain a copy of the License at:
+//
+//     https://opensource.org/licenses/MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so, subject
+// to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+// AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #ifndef _IRIS_UNORDERED_MAP_
 #define _IRIS_UNORDERED_MAP_
 
@@ -10,7 +36,7 @@ template <typename _Key, typename _Val, typename _Al = iris::allocator<iris::pai
 class unordered_map : protected hashtable<_Key, iris::pair<const _Key, _Val>, _Al, iris::map_key_extract> {
 	typedef typename _Al::value_type _Myal_value_type;
 
-	typedef hashtable<_Key, iris::pair<const _Key, _Val>, _Al, iris::map_key_extract>			 _Mytable;
+	typedef hashtable<_Key, iris::pair<const _Key, _Val>, _Al, iris::map_key_extract> _Mytable;
 	typedef typename _Mytable::_Bucket_alloc_type _Bucket_alloc_type;
 
 public:
